a4ed855507119de19c1d0cf36786fc6bfc8ba32d
AriMar159 <96703023+AriMar159@users.noreply.github.com>
2024-11-18 19:58:01 -0500

    new changes

--- main.js
+++ main.js
@@ -40,3 +40,57 @@ for (let i = 1; i <= 100; i++) {
         console.log(i);
     }
 }
+
+let n = 10
+
+function isPrime(num) {
+    if(num <= 1) return false;
+    for (let i = 2; i < Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+
+let nextPrime = n + 1;
+while (true) {
+    if (isPrime(nextPrime)) {
+        console.log('The next prime number after ${n} is ${nextPrime}');
+        break;
+    }
+   nextPrime++;
+}
+
+const csvString = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26"
+let cell = "";
+let row = [];
+let csvArray = [];  
+let numColumns = 0; 
+
+for (let i = 0; i <csvString.length; i++) {
+  const char = csvString[i];
+
+  if (char === ",") {
+    row.push(cell);
+    cell = "";
+  } else if (char === "\n") {
+    row.push(cell);
+    
+    if (numColumns === 0) {
+      numColumns = row.length; 
+    }
+    csvArray.push(row);
+
+    cell = "";
+    row = [];
+  } else { cell += char;
+  }
+}
+
+if (cell) {
+  row.push(cell);
+  csvArray.push(row);
+}
+
+console.log(csvArray);
+
+console.log("Number of columns:", numColumns);
